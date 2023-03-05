a96e2a74e5e6aeeb2a31b7446890f28a0d0a8358
mouradchahian <mouradchahiane@gmail.com>
2023-03-05 02:59:46 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
+die('mourad');
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
